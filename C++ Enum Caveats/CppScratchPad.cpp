f079a6238fadff587155441a3511bcce16a89826
Kulpot <kulpotwahu@gmail.com>
2023-12-16 17:46:02 +0800

    Add project files.

---
+++ C++ Enum Caveats/CppScratchPad.cpp
@@ -0,0 +1,3 @@
+// ------------------------------- C++ Enum Caveats -------------------------------
+// ref link:https://www.youtube.com/watch?v=NeI6G_4Rkkw&list=PLRwVmtr-pp056ahRbFrYbeAnIu2qvXPFe&index=9
+
