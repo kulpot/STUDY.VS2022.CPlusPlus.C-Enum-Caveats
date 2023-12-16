c920c47c79b5c98d2f4555a6ab8f0bdab7b9616a
Kulpot <kulpotwahu@gmail.com>
2023-12-16 17:56:27 +0800

    ...

--- C++ Enum Caveats/CppScratchPad.cpp
+++ C++ Enum Caveats/CppScratchPad.cpp
@@ -13,42 +13,68 @@ using std::endl;
 //const int DONKEY = 2;
 //const int FISH = 3;
 
+struct AnimalsEnum		// optional scoping issue: not suggested
+{
+	enum Animals
+	{
+		SHEEP,
+		COW,
+		DONKEY,
+		FISH,
+		//MAX
+		MAX_ANIMALS		// explicit
+	};
+};
+
 enum Animals
 {
 	SHEEP,
 	COW,
 	DONKEY,
 	FISH,
-	MAX
+	//MAX
+	MAX_ANIMALS		// explicit
+};
+
+enum CarManufacturers
+{
+	FORD,
+	HONDA,
+	TOYOTA,
+	//MAX
+	MAX_CAR_MANUFACTURERS		// explicit
 };
 
 void main()
 {
-	for (int i = 0; i < Animals::MAX; i++)
+	int i = Animals::SHEEP;
+	//for (int i = 0; i < Animals::MAX; i++)
+	//for (int i = 0; i < MAX; i++)
+	for(int i = 0; i < AnimalsEnum::Animals::MAX_ANIMALS; i++)
 	{
 
 	}
 
-	Animals a = Animals::FISH;
-	cout << COW << endl;
-	cout << a << endl;
-	int i = a;
-	cout << i << endl;
-	Animals b = static_cast<Animals>(i);
-	cout << b << endl;
-
-	int myAnimal = DONKEY;
-	// ...
-	if (myAnimal == FISH)
-	{
-
-	}
-	else if (myAnimal == DONKEY)
-	{
-
-	}
-	else
-	{
-
-	}
+	//Animals a = Animals::FISH;
+	//cout << COW << endl;
+	//cout << a << endl;
+	//int i = a;
+	//cout << i << endl;
+	//Animals b = static_cast<Animals>(i);
+	//cout << b << endl;
+	//
+	//int myAnimal = DONKEY;
+	//// ...
+	//if (myAnimal == FISH)
+	//{
+	//
+	//}
+	//else if (myAnimal == DONKEY)
+	//{
+	//
+	//}
+	//else
+	//{
+	//
+	//}
 }
